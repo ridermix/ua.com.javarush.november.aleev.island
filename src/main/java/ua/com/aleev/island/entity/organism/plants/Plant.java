0cb5a53818827a1e84dfe8d5a35bb19ce9e7c9a8
rustekapl <rustekapl@gmail.com>
2022-12-26 09:36:59 +0200

    Implement OrganismTypeData annotation

---
+++ src/main/java/ua/com/aleev/island/entity/organism/plants/Plant.java
@@ -0,0 +1,46 @@
+package ua.com.aleev.island.entity.organism.plants;
+
+import ua.com.aleev.island.annotation.OrganismTypeData;
+import ua.com.aleev.island.entity.map.Location;
+import ua.com.aleev.island.entity.organism.Limit;
+import ua.com.aleev.island.entity.organism.Organism;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+@OrganismTypeData(name = "Plant", icon = "\uD83C\uDF3F", maxWeight = 1, maxCount = 200, maxSpeed = 0, maxFood = 0)
+public class Plant extends Organism {
+
+    public Plant(String name, String icon, double weight, Limit limit) {
+        super(name, icon, limit);
+    }
+
+    @Override
+    public void spawn(Location currentLocation) {
+        Location nextLocation = currentLocation.getNextLocations(1);
+        safePlantSpawn(nextLocation);
+    }
+
+    private void safePlantSpawn(Location currentLocation) {
+        currentLocation.getLock().lock();
+        try {
+            Set<Organism> plants = currentLocation.getResidents().get(this.getType());
+
+            if(plants !=null){
+                if (plants.size() < this.getLimit().getMaxCount()) {
+                    Organism clone = this.clone();
+                    clone.setWeight(this.getLimit().getMaxWeight());
+                    plants.add(clone);
+                }
+            } else {
+                Map<String, Set<Organism>> residents = currentLocation.getResidents();
+                residents.put(this.getType(), new HashSet<>());
+                Set<Organism> organisms = residents.get(getType());
+                organisms.add(this);
+            }
+        } finally {
+            currentLocation.getLock().unlock();
+        }
+    }
+}
