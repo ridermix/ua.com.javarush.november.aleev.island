7f9c2b139063004cd8564d51ae8d096a26def4a0
rustekapl <rustekapl@gmail.com>
2022-12-26 09:47:41 +0200

    Implement service

---
+++ src/main/java/ua/com/aleev/island/service/Task.java
@@ -0,0 +1,21 @@
+package ua.com.aleev.island.service;
+
+import ua.com.aleev.island.entity.organism.Organism;
+
+import java.util.function.Consumer;
+
+public class Task {
+    private final Organism organism;
+    private final Consumer<Organism> operation;
+
+
+    public Task(Organism organism, Consumer<Organism> operation) {
+        this.organism = organism;
+        this.operation = operation;
+    }
+
+    public void run(){
+
+        operation.accept(organism);
+    }
+}
